fd4d86272ecfa398a1aacdbb3fa333eec23249a2
Elias <elabd.elias@gmail.com>
2025-03-12 18:32:57 +0100

    Add GameState enumerators

--- include/ui/GameState.h
+++ include/ui/GameState.h
@@ -2,9 +2,10 @@
 #define GAMESTATE_H
 
 enum class GameState {
-    // More game states will be added
-    SELECTION_MENU,
-    END,
+    MainMenu,
+    Selection,
+    Battle,
+    Exit
 };
 
 #endif
