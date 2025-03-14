e63908f81b725149b6a9975c42576eb7a28054c5
Elias <elabd.elias@gmail.com>
2025-03-14 20:48:04 +0100

    Add introduction menu + user name input and redirection

---
+++ include/ui/Introduction.h
@@ -0,0 +1,10 @@
+#ifndef INTRODUCTION_MENU_H
+#define INTRODUCTION_MENU_H
+
+#include "GameState.h"
+
+#include "ftxui/component/screen_interactive.hpp"
+
+std::string IntroductionMenu(ftxui::ScreenInteractive& screen, GameState& current_state);
+
+#endif
