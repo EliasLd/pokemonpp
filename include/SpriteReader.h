283127aebde711bccdafc7b5ec2fe4194493610f
unsigned0741 <circlingammonia760@simplelogin.co>
2025-04-04 19:13:54 +0200

    Add header file `SpriteReader.h` with sprite data dir path

---
+++ include/SpriteReader.h
@@ -0,0 +1,8 @@
+#ifndef SPRITEREADER_H
+#define SPRITEREADER_H
+
+#include <string_view>
+
+inline constexpr std::string_view SPRITE_DIR = "../data/sprites/small/regular/";
+
+#endif // SPRITEREADER_H
