600ddc441961d2cc6707b641211827e607cb3dc4
unsigned0741 <circlingammonia760@simplelogin.co>
2025-05-22 20:36:08 +0200

    Refactor Game.h to introduce GameController class
    - Introduced GameController class to encapsulate game logic and state.
    - Included necessary headers for Trainer, ftxui, unordered_map, and vector.
    - Changed function `runGame()` to a method of GameController class.
    - Added member variables for GameState, pokemons, leaders, masters, Player, and screen.

--- include/ui/Game.h
+++ include/ui/Game.h
@@ -1,6 +1,12 @@
 #ifndef GAMESTATE_H
 #define GAMESTATE_H
 
+#include "Trainer.h"
+
+#include "ftxui/component/screen_interactive.hpp"
+
+#include <unordered_map>
+#include <vector>
 #include <string>
 
 enum class GameState {
@@ -12,7 +18,20 @@ enum class GameState {
     EndMenu,
 };
 
-void runGame();
+class GameController {
+public:
+    GameController();
+    void runGame();
+    void resetToGameStart();
+
+private:
+    GameState state { GameState::StartMenu };
+    std::unordered_map<std::string, std::shared_ptr<Pokemon>> pokemons;
+    std::vector<GymLeader> leaders;
+    std::vector<Master> masters;
+    Player player {};
+    ftxui::ScreenInteractive screen { ftxui::ScreenInteractive::Fullscreen() };
+};
 
 extern const std::string pokemon_filename;
 extern const std::string leader_filename;
